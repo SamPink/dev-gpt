984f69a038b7e50b9647572a2cc166f35cb1c69e
spink-eth <spink@metaverseholdings.tech>
2023-03-17 10:27:39 +0000

    improved code running

--- app.py
+++ app.py
@@ -23,56 +23,62 @@ class GPT4:
         return response
 
     def extract_code_from_response(self, response: str):
-        code_pattern = re.compile(r'```(?:python)?\s*(.*?)```', re.DOTALL)
-        match = code_pattern.search(response)
-        if match:
-            return match.group(1)
-        return None
+        code_blocks = re.findall(r'```(?:python|bash)?\s*[\s\S]*?```', response)
+
+        extracted_code = {
+            "python": [],
+            "bash": []
+        }
+
+        for code_block in code_blocks:
+            code_type, code = re.match(r'```(python|bash)?\s*([\s\S]*?)```', code_block).groups()
+            code_type = code_type or "python"
+            extracted_code[code_type].append(code.strip())
+
+        return extracted_code
 
     def write_message_to_file(self, filename: str, message: str):
         code = self.extract_code_from_response(message)
         if code:
             with open(filename, "w") as f:
-                f.write(code)
+                f.write("\n".join(code["python"]))
+
+    def install_dependencies(self, dependencies: list):
+        for dep in dependencies:
+            subprocess.run(['pip', 'install', dep])
 
     def generate_and_save_response(self, output_filename: str):
         response = self.create_chat_completion()
         self.write_message_to_file(output_filename, response)
+        code = self.extract_code_from_response(response)
+        self.install_dependencies(code["bash"])
 
     def run_code_and_add_output_to_messages(self, filename: str):
         result = subprocess.run(['python', filename], capture_output=True, text=True)
         output = result.stdout.strip()
         if output:
-              #add a message saying I run the code and this is the output
             self.add_message("I ran the code and this is the output:", "system")
             self.add_message(output, "system")
 
 if __name__ == "__main__":
     gpt4 = GPT4(config.OPENAI_API_KEY)
-    
-    # Ask GPT-4 to act as a data engineer
-    gpt4.add_message("Act as a data engineer.", role="system")
-    
-    # Ask GPT-4 to write Python code
-    gpt4.add_message("Write a Python script to read a rest api and print its contents.")
-    gpt4.add_message("get the price of eth in usd")
-    
-    #create a folder called output and add a file called output.py
-    output_filename = "output/output.py"
-    gpt4.generate_and_save_response(output_filename)
-    gpt4.run_code_and_add_output_to_messages(output_filename)
-    
-    gpt4.add_message("Update the code so it also gets the historical price of eth in usd for the last 30 days and writes it to a csv file.")
-    
-    output_2_filename = "output/output_2.py"
-    gpt4.generate_and_save_response(output_2_filename)
-    gpt4.run_code_and_add_output_to_messages(output_2_filename)
-    
-    #now update the code so it also gets btc proces, update it to use sqlalchemy and write the data to a database
-    gpt4.add_message("Update the code so it also gets the price of btc in usd and writes it to a database using sqlalchemy.")
-    
-    output_3_filename = "output/output_3.py"
-    gpt4.generate_and_save_response(output_3_filename)
-    gpt4.run_code_and_add_output_to_messages(output_3_filename) 
-    
-    print(gpt4.messages)
+
+    # Update the initial system message to request code in the specified format
+    gpt4.add_message("Act as a data engineer and provide code in the following format: \n\n```bash\n(required dependencies)\n```\n\n```python\n(Python code)\n```\n\nProvide instructions on how to run the code in the response.", role="system")
+
+    while True:
+        user_input = input("Enter your message (type 'exit' to quit): ")
+
+        if user_input.lower() == 'exit':
+            break
+
+        gpt4.add_message(user_input)
+
+        output_filename = "output/output.py"
+        gpt4.generate_and_save_response(output_filename)
+        gpt4.run_code_and_add_output_to_messages(output_filename)
+
+        for message in gpt4.messages[-3:]:
+            print(f"{message['role'].capitalize()}: {message['content']}")
+        print("\n")
+
