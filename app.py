f3fe50291b263d6403fb30c4f9d9660e6b2522fe
spink-eth <spink@metaverseholdings.tech>
2023-03-17 15:42:16 +0000

    added example prompts

--- app.py
+++ app.py
@@ -22,21 +22,29 @@ class GPT4:
         # Create output folder if it doesn't exist
         os.makedirs(self.output_folder, exist_ok=True)
 
+    def log_message(self, message: str, role: str = "user"):
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log_message = f"{timestamp} [{role.upper()}] {message}"
+        print(log_message)
+        with open("logs.txt", "a") as log_file:
+            log_file.write(log_message + "\n")
+
     def add_message(self, message: str, role: str = "user"):
         self.messages.append({"role": role, "content": message})
+        self.log_message(message, role)
 
-    def create_chat_completion(self) -> str:
-        print("Waiting for GPT response...")
+    def create_chat_completion(self, messages: List[Dict[str, str]] = None) -> str:
+        self.log_message("Waiting for GPT response...", "system")
         start_time = time.time()
 
-        resp = openai.ChatCompletion.create(model=self.model, messages=self.messages,)
+        resp = openai.ChatCompletion.create(model=self.model, messages=messages or self.messages)
 
         response = resp["choices"][0]["message"]["content"]
         self.add_message(response, "assistant")
 
         end_time = time.time()
         elapsed_time = end_time - start_time
-        print(f"GPT response received in {elapsed_time:.2f} seconds")
+        self.log_message(f"GPT response received in {elapsed_time:.2f} seconds", "system")
 
         return response
 
@@ -54,6 +62,10 @@ class GPT4:
 
         return extracted_code
 
+    def extract_filename_from_query(self, response: str) -> str:
+        return self.create_chat_completion([{"role": "user", "content": f"genrrate file name for this project make sure its a valide windows foldername: {response}"}])[:-1]
+
+    
     def write_message_to_file(self, filename: str, message: str):
         code = self.extract_code_from_response(message)
         if code:
@@ -95,9 +107,6 @@ class GPT4:
                 break
 
 
-
-
-
 def submit_query():
     selected_query = query_var.get()
     if selected_query == "Custom":
@@ -107,9 +116,14 @@ def submit_query():
 
     query_entry.delete(0, tk.END)
     gpt4.add_message(user_query)
+    
+    folder_name = gpt4.extract_filename_from_query(user_query)
+    
+    query_folder = f"{gpt4.output_folder}/{folder_name}"
+    
+    os.makedirs(query_folder, exist_ok=True)
 
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    output_filename = f"output/{timestamp}_{len(gpt4.messages)}.py"
+    output_filename = f"{query_folder}/code.py"
     gpt4.generate_and_save_response(output_filename)
     gpt4.run_code_and_add_output_to_messages(output_filename)
     update_message_display()
@@ -121,14 +135,20 @@ def update_message_display():
         message_display.insert(
             tk.END, f"{message['role'].capitalize()}: {message['content']}\n"
         )
+    message_display.see(tk.END)  # Scroll to the bottom
 
 
 if __name__ == "__main__":
     gpt4 = GPT4(config.OPENAI_API_KEY)
     gpt4.add_message(
-        "Act as a data engineer and provide code in the following format: \n\n```bash\n(required dependencies)\n```\n\n```python\n(Python code)\n```\n\n just output the required format, nothing else.",
+        "Please provide a code snippet in the following format:\n\n"
+        "```bash\n(required dependencies)\n```\n\n"
+        "```python\n(Python code)\n```\n\n"
+        "And also suggest a filename in the following format:\n"
+        "file name: ([\\w\\-]+)",
         role="system",
     )
+
 
     # Create the UI
     root = tk.Tk()
@@ -143,9 +163,19 @@ if __name__ == "__main__":
     query_var = tk.StringVar()
     query_options = [
         "Custom",
-        "write a python script to get the current eth price in £.",
-        "now update the script to get 7 days of historical data for eth in £",
-        # Add more predefined queries here...
+        "Write a program that plays Rock-Paper-Scissors against the user.",
+        "Create a web scraper that fetches data from a website and saves it to a CSV file.",
+        "Build a GUI application that converts units of measurement (e.g. miles to kilometers).",
+        "Write a program that generates a random password based on user preferences (e.g. length, complexity).",
+        "Implement a chatbot that can answer questions on a specific topic using machine learning.",
+        "Create a program that generates a maze and allows the user to solve it using arrow keys.",
+        "Build a simple game using Pygame library (e.g. Snake, Tetris, Space Invaders).",
+        "Create a program that extracts data from a PDF document and saves it to a text file.",
+        "Build a program that fetches real-time stock market data and displays it in a graph.",
+        "Write a program that generates a word cloud from a text file.",
+        "Create a program that generates a random image and saves it to a file.",
+        "Build a program that detects faces in an image using computer vision.",
+        "Write a program that scrapes data from social media sites (e.g. Twitter, Facebook).",
     ]
     query_dropdown = ttk.OptionMenu(mainframe, query_var, query_options[0], *query_options)
     query_dropdown.grid(column=0, row=1, sticky=(tk.W, tk.E))
--- output/20230317_140701_2.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_eth_price():
-    url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=GBP"
-    response = requests.get(url)
-    eth_price_gbp = response.json()['GBP']
-    return eth_price_gbp
-
-print(f"Current ETH price in £: {get_eth_price()}")
